32a5750b4e44835c18cfa0d2a57b36077e3e7070
byhaqii <rizalbaihaqiiiiii@gmail.com>
2023-12-14 11:24:37 +0700

    Question 2 pertanyaan 5

--- Square.java
+++ Square.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Square {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        System.out.println("Masukkan nilai N = ");
+        System.out.print("Masukkan nilai N = ");
         int N = scan.nextInt();
         for (int iOuter = 1; iOuter <= N; iOuter++) {
             for (int i = 1; i <= N; i++) {
