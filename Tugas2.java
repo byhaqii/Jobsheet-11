ef628eab6932d3e3f97dd9dac3319ef076f01921
byhaqii <rizalbaihaqiiiiii@gmail.com>
2023-12-14 11:53:15 +0700

    Tugas 2 no 2

---
+++ Tugas2.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Tugas2 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Masukkan nilai N = ");
+        int N = scan.nextInt();
+        if (N >= 5) {
+            int i = N;
+            while (i >= 1) {
+                int j = 1;
+                while (j <= i) {
+                    System.out.print("*");
+                    j++;
+                }
+                System.out.println();
+                i--;
+            }
+        } else {
+            System.out.println("minimal input 5 ");
+        }
+
+    }
+}
